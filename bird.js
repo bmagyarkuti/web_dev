688be7e38146c25cc16dc22e50aca63c798898ce
Barna Magyarkuti <bmagyarkuti@gmail.com>
2019-10-15 09:10:20 +0200

    modules

---
+++ bird.js
@@ -0,0 +1,9 @@
+export default class Bird {
+  constructor() {
+    this._position = 200;
+  }
+
+  get position() {
+    return this._position;
+  }
+}
--- flappy.js
+++ flappy.js
@@ -1,5 +1,18 @@
+import Bird from './bird.js';
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+const bird = new Bird();
+
+const image = new Image();
+image.src = './bird.png';
+
+const render = function() {
+  ctx.drawImage(image, bird.position, 100, 50, 50);
+}
+
+image.onload = render;
 
 ctx.fillRect(5, 5, 20, 100)
 ctx.strokeRect(30, 5, 20, 100)
